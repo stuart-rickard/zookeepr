509d65b36dbb90946ded987cea33956be812a39b
stuart-rickard <stuart@bau-dev.com>
2022-01-17 15:47:55 -0800

    downloaded code snapshot

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -13,12 +13,12 @@ router.get('/zookeepers', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
+router.get('/aquarium', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/aquarium.html'));
+});
+
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
-
-const animalRoutes = require('../apiRoutes/animalRoutes');
-
-router.use(animalRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-const apiRoutes = require('./routes/apiRoutes')
-const htmlRoutes = require('./routes/htmlRoutes');
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.use(express.static('public'));
-
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
